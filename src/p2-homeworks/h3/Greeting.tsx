98846df8f8f6ac6ace81399f18e767ee447700b0
serhii <romanchuk.sss22121999@gmail.com>
2020-11-24 16:55:20 +0200

    03 task

--- src/p2-homeworks/h3/Greeting.tsx
+++ src/p2-homeworks/h3/Greeting.tsx
@@ -1,28 +1,37 @@
-import React from "react";
-import s from "./Greeting.module.css";
+import React, {ChangeEvent, KeyboardEvent} from 'react';
+import s from './Greeting.module.css';
 
 type GreetingPropsType = {
-    name: any // need to fix any
-    setNameCallback: any // need to fix any
-    addUser: any // need to fix any
-    error: any // need to fix any
-    totalUsers: any // need to fix any
+   name: string
+   setNameCallbackOnChange: (e: ChangeEvent<HTMLInputElement>) => void
+   setNameCallbackKeyPress: (e: KeyboardEvent<HTMLInputElement>) => void
+   addUser: () => void
+   error: string
+   totalUsers: number
 }
 
 // презентационная компонента (для верстальщика)
 const Greeting: React.FC<GreetingPropsType> = (
-    {name, setNameCallback, addUser, error, totalUsers} // деструктуризация пропсов
+   {name, setNameCallbackOnChange, setNameCallbackKeyPress, addUser, error, totalUsers} // деструктуризация пропсов
 ) => {
-    const inputClass = s.error; // need to fix with (?:)
 
-    return (
-        <div>
-            <input value={name} onChange={setNameCallback} className={inputClass}/>
-            <span>{error}</span>
-            <button onClick={addUser}>add</button>
-            <span>{totalUsers}</span>
-        </div>
-    );
+   return (
+      <div className={s.greeting}>
+         <div>
+            <input
+               value={name}
+               onChange={setNameCallbackOnChange}
+               onKeyPress={setNameCallbackKeyPress}
+               className={`${error && s.error} ${s.input}`}
+            />
+            <button onClick={addUser} className={s.button}>add</button>
+            <div className={s.total}>
+               <span>{totalUsers}</span>
+            </div>
+         </div>
+         <span className={error && s.errorMessage}>{error}</span>
+      </div>
+   );
 }
 
 export default Greeting;
